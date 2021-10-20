2bf3a30d18d5af42104c2b07047e5b2f078daa0d
Kyosuke Nekoyashiki <supercatexpert@gmail.com>
2021-10-20 18:04:34 +0900

    Disabled console blank in Linux.

--- include/configs/rzg2l-novotech.h
+++ include/configs/rzg2l-novotech.h
@@ -61,14 +61,15 @@
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"bootm_size=0x10000000 \0" \
-	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p1 \0" \
-	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p1 \0" \
-	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
-	"emmcload=ext4load mmc 0:1 0x4A080000 boot/Image.gz;ext4load mmc 0:1 0x48000000 boot/r9a07g044l-smarc-rzg2l.dtb;run prodemmcbootargs \0" \
-	"sd1load=ext4load mmc 1:1 0x4A080000 boot/Image.gz;ext4load mmc 1:1 0x48000000 boot/r9a07g044l-smarc-rzg2l.dtb;run prodsdbootargs \0" \
-	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
+	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 video=HDMI-A-1:1280x720@60e \0" \
+	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 video=HDMI-A-1:1280x720@60e consoleblank=0 \0" \
+	"bootimage=booti 0x48080000 - 0x48000000 \0" \
+	"emmcload=fatload mmc 0:1 0x48080000 Image;fatload mmc 0:1 0x48000000 r9a07g044l2-rzg2l-novotech.dtb;run prodemmcbootargs \0" \
+	"sd1load=fatload mmc 1:1 0x48080000 Image;fatload mmc 1:1 0x48000000 r9a07g044l2-rzg2l-novotech.dtb;run prodsdbootargs \0" \
+	"sdboot=run sd1load;run bootimage \0" \
+	"emmcboot=run emmcload;run bootimage \0"
 
-#define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
+#define CONFIG_BOOTCOMMAND	"run emmcboot"
 
 /* For board */
 /* Ethernet RAVB */
